4d25152927ef2dfac26e2463d4760ac7e741fb61
Damdinsvren2020 <56712755+Damdinsvren2020@users.noreply.github.com>
2022-11-10 00:39:04 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./container/home/home";
+import Dashboard from "./admin/container/Dashboard/Dashboard";
 
 function App() {
   return (
     <div>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/admin/container/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Dashboard.css";
+const Dashboard = () => {
+  return (
+    <div className="Dashboard">
+      <div className="Dashboard_Container">
+        <div className="Dashboard_Sidebar"></div>
+        <div className="Dashboard_Tables"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
